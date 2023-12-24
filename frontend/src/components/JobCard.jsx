06f3c4e0345d97be8e96b422fcf7ffbbb0a46a5f
Khemz-Developer <jalithapramod@gmail.com>
2023-12-24 14:44:37 +0530

    finished user job apply form get suitable job feild and position

--- frontend/src/components/JobCard.jsx
+++ frontend/src/components/JobCard.jsx
@@ -1,3 +1,91 @@
+import React, { useState } from "react";
+import "./jobcard.css";
+
+import { Link, useNavigate } from "react-router-dom";
+
+const JobCard = (props) => {
+  const navigate = useNavigate();
+  const [showDetails, setShowDetails] = useState(false);
+
+  const toggleDetails = () => {
+    setShowDetails(!showDetails);
+  };
+
+
+  const handleApplyClick = () => {
+    navigate("/apply", {
+      state: {
+        jobField: props.jobField,
+        jobPosition: props.jobPosition,
+      },
+    });
+  };
+
+  return (
+    <>
+      <div className="row pt-5">
+        <div className="card col-10 col-md-8 col-lg-6 mx-auto">
+          <div className="card-body">
+            <h5 className="card-title text-center">{props.jobField}</h5>
+            <h6 className="card-subtitle mb-3 text-muted text-center">
+              Job Position : {props.jobPosition}
+            </h6>
+            <p className="card-text text-center">{props.jobDescription}</p>
+
+            {showDetails && (
+              <div className="row">
+                <p className="card-text mx-2">Location: {props.workLocation}</p>
+                <p className="card-text mx-2">Salary: Rs: {props.salary}</p>
+                <p className="card-text mx-2">Due Date: {props.dueDate}</p>
+                <p className="card-text mx-2">
+                  Work Method: {props.workMethod}
+                </p>
+                <p className="card-text mx-2">Work Type: {props.workType}</p>
+                <p className="card-text mx-2">
+                  Required Skills: {props.requiredSkills.join(", ")}
+                </p>
+                <p className="card-text mx-2">
+                  Educational Qualifications: {props.educationalQualifications}
+                </p>
+                {/* <Link to={"/apply"}> */}
+               
+
+                <Link
+                  to={`/apply?jobField=${props.jobField}&jobPosition=${props.jobPosition}`}
+                >
+                  <button
+                    onClick={handleApplyClick}
+                    className="btn btn-outline-primary mx-2 col-2"
+                  >
+                    Apply
+                  </button>
+                  apply
+                </Link>
+              </div>
+            )}
+            <div className="row">
+              <div className="col">
+                <button
+                  className={`btn btn-outline-primary px-3 mx-2  mt-2 ${
+                    showDetails ? "btn-outline-danger" : ""
+                  }`}
+                  onClick={toggleDetails}
+                >
+                  {showDetails ? "Hide Details" : "See More"}
+                </button>
+              </div>
+              <div className="col"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default JobCard;
+
+
 // import React from "react";
 // import "./jobcard.css";
 
@@ -66,69 +154,3 @@
 // };
 
 // export default JobCard;
-
-import React, { useState } from "react";
-import "./jobcard.css";
-import { Link } from "react-router-dom";
-
-
-const JobCard = (props) => {
-  const [showDetails, setShowDetails] = useState(false);
-
-  const toggleDetails = () => {
-    setShowDetails(!showDetails);
-  };
-
-  return (
-    <>
-      <div className="row pt-5">
-        <div className="card col-10 col-md-8 col-lg-6 mx-auto">
-          <div className="card-body">
-            <h5 className="card-title text-center">{props.jobField}</h5>
-            <h6 className="card-subtitle mb-3 text-muted text-center">
-              Job Position : {props.jobPosition}
-            </h6>
-            <p className="card-text text-center">{props.jobDescription}</p>
-
-            {showDetails && (
-              <div className="row">
-                
-                <p className="card-text mx-2">Location: {props.workLocation}</p>
-                <p className="card-text mx-2">Salary: Rs: {props.salary}</p>
-                <p className="card-text mx-2">Due Date: {props.dueDate}</p>
-                <p className="card-text mx-2">Work Method: {props.workMethod}</p>
-                <p className="card-text mx-2">Work Type: {props.workType}</p>
-                <p className="card-text mx-2">
-                  Required Skills: {props.requiredSkills.join(", ")}
-                </p>
-                <p className="card-text mx-2">
-                   Educational Qualifications: {props.educationalQualifications}
-                </p>
-                <Link to={"/apply"}>
-                  <button className="btn btn-outline-primary  mx-2 col-2">
-                    Apply
-                  </button>
-                </Link>
-              </div>
-            )}
-            <div className="row">
-              <div className="col">
-                <button
-                  className={`btn btn-outline-primary px-3 mx-2  mt-2 ${
-                    showDetails ? "btn-outline-danger" : ""
-                  }`}
-                  onClick={toggleDetails}
-                >
-                  {showDetails ? "Hide Details" : "See More"}
-                </button>
-              </div>
-              <div className="col"></div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
-
-export default JobCard;
--- frontend/src/components/usersidepages/UserJopApply.jsx
+++ frontend/src/components/usersidepages/UserJopApply.jsx
@@ -1,23 +1,95 @@
-import React from 'react'
-import UserSidebar from '../UserSidebar'
-import ApplicationForm from './ApplicationForm'
+import React, { useEffect } from "react";
+import { Button, Container, Form } from "react-bootstrap";
+import UserSidebar from "../UserSidebar";
+import { useLocation, useNavigate } from "react-router-dom";
+
 const UserJopApply = () => {
+  const location = useLocation(); // Get the current location
+  const params = new URLSearchParams(location.search);
+  const jobField = params.get("jobField");
+  const jobPosition = params.get("jobPosition");
+  const navigate = useNavigate(); // Add this line
+
+  // console.log("Location state:", location.state);
+  // console.log("Received job details in UserJobApply:", {
+  //   jobField,
+  //   jobPosition,
+  // });
+
+  useEffect(() => {
+    console.log("UserJopApply component rendered.");
+    console.log("Received job details in UserJobApply:", {
+      jobField,
+      jobPosition,
+    });
+
+    // Check if jobField or jobPosition is missing
+    if (!jobField || !jobPosition) {
+      // Navigate back to "/uservacancy"
+      navigate("/uservacancy");
+    }
+  }, [jobField, jobPosition, navigate]);
+
   return (
     <div>
-        <br></br>
-        <br></br>
-        <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
       <UserSidebar>
         <br></br>
         <br></br>
-        <ApplicationForm/>
+        {/* <ApplicationForm/> */}
+        <div>
+          <Container>
+            <Form>
+              <h4 className="text-center mt-4">Application Form</h4>
+
+              <Form.Group className="mb-3" controlId="formJobFeild">
+                <Form.Label>Job Field</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="jobFeild"
+                  value={jobField} // Set the value from the state
+                  readOnly // Make the field read-only if you want to prevent user input
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formJobDescription">
+                <Form.Label>Job Position</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="jobPosition"
+                  value={jobPosition} // Set the value from the state
+                  readOnly // Make the field read-only if you want to prevent user input
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formSalary">
+                <Form.Label>Salary</Form.Label>
+                <Form.Control
+                  type="number"
+                  placeholder="Salary"
+                  name="salary"
+                  required
+                />
+              </Form.Group>
+
+              {/* Due Date */}
+              <Form.Group className="mb-3" controlId="formDateofBirth">
+                <Form.Label>Date of Birth</Form.Label>
+                <Form.Control type="date" name="dueDate" required />
+              </Form.Group>
+
+              <Button type="submit" variant="primary" className="mt-3">
+                Submit
+              </Button>
+            </Form>
+          </Container>
+        </div>
         <br></br>
         <br></br>
       </UserSidebar>
-      
     </div>
-  )
-}
+  );
+};
 
-export default UserJopApply
-
+export default UserJopApply;
